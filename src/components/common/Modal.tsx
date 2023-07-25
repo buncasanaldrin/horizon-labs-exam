c5272957d92ab013b5f24487451bca86b6079f23
buncasanaldrin <buncasanaldrin@gmail.com>
2023-07-25 23:21:56 +0800

    Initial Commit

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -9,38 +9,36 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ children, isOpen, closeModal }) => {
-  return (
-    isOpen && (
-      <div className="fixed z-10 inset-0 overflow-y-auto">
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          <div className="fixed inset-0 transition-opacity">
-            <div
-              className="absolute inset-0 bg-gray-500 opacity-75"
-              onClick={closeModal}
-            ></div>
-          </div>
-
+  return isOpen ? (
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity">
           <div
-            className="inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all max-w-lg md:max-w-2xl sm:my-8 sm:align-middle sm:w-full"
-            role="dialog"
-            aria-modal="true"
-            aria-labelledby="modal-headline"
-          >
-            <div className="absolute right-0 p-6">
-              <Button intent="ghost" onClick={closeModal}>
-                X
-              </Button>
-            </div>
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div className="mt-3 text-center sm:mt-0 sm:text-left">
-                {children}
-              </div>
+            className="absolute inset-0 bg-gray-500 opacity-75"
+            onClick={closeModal}
+          ></div>
+        </div>
+
+        <div
+          className="inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all max-w-lg md:max-w-2xl sm:my-8 sm:align-middle sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div className="absolute right-0 p-6">
+            <Button intent="ghost" onClick={closeModal}>
+              X
+            </Button>
+          </div>
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="mt-3 text-center sm:mt-0 sm:text-left">
+              {children}
             </div>
           </div>
         </div>
       </div>
-    )
-  );
+    </div>
+  ) : null;
 };
 
 export default Modal;
